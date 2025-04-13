cab656e750409eff38ff47cefda262d840c23ba3
chandracs238 <chandhu7013@gmail.com>
2025-04-13 17:00:58 +0530

    small update

--- src/main/java/com/workout/tracker/controller/WorkoutPlanController.java
+++ src/main/java/com/workout/tracker/controller/WorkoutPlanController.java
@@ -14,7 +14,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
